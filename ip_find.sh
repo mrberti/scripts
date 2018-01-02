50ff174e8d01c8ed9f2453b0cb73a2436a439fd3
Simon Bertling <simon.bertling@gmx.de>
2018-01-02 17:36:13 +0100

    improved script

--- ip_find.sh
+++ ip_find.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
+# This little scripts tries to get answers from all devices on the net
+# Argument one defines a subdomain, e.g. 192.168.123
 
 # depending on the type of ping, use these options and grep string
-if [ -n "$(type ping | grep WINDOWS)" ]; then
-	options="-n 1 -w 1000"
+if [ -n "$(env | grep -i windows)" ]; then
+	echo "Using windows mode"
+	options="-n 1 -w 500"
 	grep_str="Antwort"
+	base_ip=`ipconfig | grep IPv4 | grep -Eo '192.168.[0-9]{1,3}' | tail -1`
 else
+	echo "Using unix mode"
 	options="-c 1"
-	grep_str="bytes from"
+	if [ -n "$(echo $LANG | grep de)" ]; then
+		grep_str="bytes from"
+	else
+		# assuming english
+		grep_str="bytes from"
+	fi
+	base_ip=`ifconfig | grep -Eo '192.168.[0-9]{1,3}' | head -1`
 fi
 
-# create range for ips
-is=`seq 1 254`
+if [ "$1" != "" ]; then
+	echo "using script argument $1"
+	exit
+	base_ip=$1
+fi
+
+echo "start probing ip on domain $base_ip"
 
 # ping range of ips
-for i in $is;
+for i in `seq 1 254`;
 do
-	ip="192.168.1.$i"
+	ip="$base_ip.$i"
 	ping $options $ip | grep "$grep_str"
 	echo -ne $i '\r'
 done
