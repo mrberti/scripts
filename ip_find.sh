97563c228d2964d2c1e5e17558c0dc5fcb35bbec
Simon Bertling <simon.bertling@gmx.de>
2018-03-07 10:41:11 +0900

    made compatible with english windows ping

--- ip_find.sh
+++ ip_find.sh
@@ -7,7 +7,7 @@
 if [ -n "$(env | grep -i windows)" ]; then
 	echo "Using windows mode"
 	options="-n 1 -w 1000"
-	grep_str="Antwort"
+	grep_str="(Antwort)|(Reply)"
 	base_ip=`ipconfig | grep IPv4 | grep -Eo '192.168.[0-9]{1,3}' | tail -1`
 else
 	echo "Using unix mode"
@@ -33,6 +33,6 @@ echo "start probing ip on domain $base_ip"
 for i in `seq 1 254`;
 do
 	ip="$base_ip.$i"
-	ping $options $ip | grep "$grep_str"
+	ping $options $ip | grep -iE "$grep_str"
 	echo -ne $i '\r'
 done
