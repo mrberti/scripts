06c35d7c5cf18fcec6578d889e75eae02d37d5f5
Simon Bertling <simon.bertling@gmx.de>
2020-05-12 05:17:51 +0900

    updated rabbitmq docker script

--- rabbitmq/docker_run_rabbitmq.sh
+++ rabbitmq/docker_run_rabbitmq.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
+## You need to enable ipforwarding:
+# sysctl net.ipv4.conf.all.forwarding=1
+# sudo iptables -P FORWARD ACCEPT
 
 set -e 
 
 DOCKER_IMG="rabbitmq:3-management"
 DOCKER_NAME="rabbitmq"
-TZ="Asia/Tokyo"
 
 docker run -d \
     --name $DOCKER_NAME \
-	--net=host \
+	-p 15672:15672 \
+	-p 1883:1883 \
 	--mount type=bind,source="/home/simon/rabbitmq/etc/rabbitmq",target="/etc/rabbitmq" \
 	--mount type=bind,source="/home/simon/rabbitmq/var/lib/rabbitmq",target="/var/lib/rabbitmq" \
 	--mount type=bind,source="/home/simon/certs",target="/etc/certs",readonly \
-	-e TZ="${TZ}" \
 	--restart=unless-stopped \
     $DOCKER_IMG \
 
