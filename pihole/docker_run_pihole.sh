39bd482119e2b9850a3c281b95aa976280bbe195
Simon Bertling <simon.bertling@gmx.de>
2022-02-19 16:16:42 +0100

    changed host interface

--- pihole/docker_run_pihole.sh
+++ pihole/docker_run_pihole.sh
@@ -5,7 +5,7 @@ set -e
 DOCKER_IMG="pihole/pihole:latest"
 DOCKER_CONFIGS="/home/simon/pihole/etc"
 DOCKER_WWW="/var/www/html/simon"
-TZ="Asia/Tokyo"
+TZ="Europe/Berlin"
 DNS1="1.1.1.1"
 DNS2="1.0.0.1"
 
@@ -16,21 +16,23 @@ echo "IP: ${IP} - IPv6: ${IPv6}"
 
 docker run -d \
     --name pihole \
-	--restart=unless-stopped\
-	--net=host\
-	--cap-add=NET_ADMIN\
+	--restart=unless-stopped \
+	-p 53:53/tcp -p 53:53/udp -p 8001:80 \
 	-v "${DOCKER_CONFIGS}/pihole/:/etc/pihole/" \
 	-v "${DOCKER_CONFIGS}/dnsmasq.d/:/etc/dnsmasq.d/" \
-	-v "${DOCKER_WWW}:/var/www/html/simon" \
-	-v "/var/www/html/.well-known:/var/www/html/.well-known" \
 	-e TZ=$TZ\
 	-e DNS1=$DNS1\
 	-e DNS2=$DNS2\
-	-e DNSMASQ_LISTENING="local"\
-	-e INTERFACE="eth0"\
-	-e ServerIPv6="${IPv6}"\
-	-e ServerIP="${IP}" \
     $DOCKER_IMG
+	# -e ServerIPv6="${IPv6}"\
+	# -e ServerIP="${IP}" \
+# -p 67/67/udp # for DHCP
+	# --cap-add=NET_ADMIN\
+	# --net=host\
+	# -v "${DOCKER_WWW}:/var/www/html/simon" \
+	# -v "/var/www/html/.well-known:/var/www/html/.well-known" \
+	# -e DNSMASQ_LISTENING="local"\
+	# -e INTERFACE="eth0"\
 #	--mount type=bind,source="/home/simon/pihole/etc/lighttpd/external.conf",target="/etc/lighttpd/external",readonly \
 #	--mount type=bind,source="/home/simon/certs",target="/etc/certs",readonly \
 #-p 53:53/tcp -p 53:53/udp -p 80:80\
